e9eb3a2f773de790f047e4ad0e46bee41b215978
BlashkoHenadzi <47740678+BlashkoHenadzi@users.noreply.github.com>
2019-12-09 00:20:03 +0300

    Добавьте файлы проекта.

---
+++ Mpp-5Lab--DependencyInjectionContainer-/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mpp_5Lab__DependencyInjectionContainer_
+{
+    public class Class1
+    {
+    }
+}
